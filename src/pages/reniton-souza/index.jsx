9678a6dd8a68cf225397df570c6b100bcfdeb579
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-03-19 11:10:14 -0300

    Update

--- src/pages/reniton-souza/index.jsx
+++ src/pages/reniton-souza/index.jsx
@@ -1,13 +1,21 @@
 'use client'
 import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 export default function reniton(){
     const router = useRouter()
     const reniton = 'https://api.whatsapp.com/send?phone=5528999796807&text=Ol%C3%A1!%20Quero%20mais%20informa%C3%A7%C3%B5es%20do%20Tempest%20Blue.'
 
+
+    setInterval(() => router.push(reniton), 2000)
+
+    
+
     return(
         <>
-            <div onLoad={() => router.push(reniton)}></div> 
+            <div className={`flex justify-center items-center h-screen transition-opacity`}>
+                <p className={`text-xxl`}>Reniton Souza</p>
+            </div> 
         </>
     )
 }
