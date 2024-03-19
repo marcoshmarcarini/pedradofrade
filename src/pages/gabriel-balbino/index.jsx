c04ab8a21a5f2ab3899240d0f76c9afbad0ec323
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-03-19 11:22:09 -0300

    update

--- src/pages/gabriel-balbino/index.jsx
+++ src/pages/gabriel-balbino/index.jsx
@@ -3,11 +3,14 @@ import { useRouter } from "next/router"
 
 export default function gabriel(){
     const router = useRouter()
-    const reniton = 'https://api.whatsapp.com/send?phone=5528999919177&text=Ol%C3%A1!%20Quero%20mais%20informa%C3%A7%C3%B5es%20do%20Vicent%20Black.'
+    const gabriel = 'https://api.whatsapp.com/send?phone=5528999919177&text=Ol%C3%A1!%20Quero%20mais%20informa%C3%A7%C3%B5es%20do%20Vicent%20Black.'
 
+    setInterval(() => router.push(gabriel), 500)
     return(
         <>
-            <div onLoad={() => router.push(reniton)}></div> 
+            <div className={`flex justify-center items-center h-screen transition-opacity`}>
+                <p className={`text-xxl`}>Gabriel Balbino</p>
+            </div>
         </>
     )
 }
--- src/pages/romulo-oliosa/index.jsx
+++ src/pages/romulo-oliosa/index.jsx
@@ -3,11 +3,15 @@ import { useRouter } from "next/router"
 
 export default function romulo(){
     const router = useRouter()
-    const reniton = 'https://api.whatsapp.com/send?phone=5528999832160&text=Ol%C3%A1!%20Quero%20mais%20informa%C3%A7%C3%B5es%20do%20Lunar%20.'
+    const romulo = 'https://api.whatsapp.com/send?phone=5528999832160&text=Ol%C3%A1!%20Quero%20mais%20informa%C3%A7%C3%B5es%20do%20Lunar%20.'
+
+    setInterval(() => router.push(romulo), 500)
 
     return(
         <>
-            <div onLoad={() => router.push(reniton)}></div> 
+            <div className={`flex justify-center items-center h-screen transition-opacity`}>
+                <p className={`text-xxl`}>Rômulo Oliosa</p>
+            </div>  
         </>
     )
 }
